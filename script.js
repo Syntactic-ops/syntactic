b303e36132f1cdd3189bce6e29cfc6be3cd2ddb2
Syntactic-ops <ben@syntactic.co.nz>
2024-06-17 18:47:09 +1200

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const logo = document.getElementById("logo");
     const newLogo = document.getElementById("new-logo");
     const paletteDots = document.querySelectorAll(".palette-dot");
+    const textContainer = document.querySelector(".text-container");
 
     const palettes = {
         blue: {
             logo: "images/logo-blue.png",
             backgroundColor: "#f2f1ec",
+            textColor: "#365b6d",
         },
         green: {
             logo: "images/logo-lightgreen.png",
             backgroundColor: "#cbddd1",
+            textColor: "#272727",
         },
         red: {
             logo: "images/logo-darkgreen.png",
             backgroundColor: "#122620",
+            textColor: "#fffef9",
         },
         purple: {
             logo: "images/logo-purple.png",
             backgroundColor: "#2b192e",
+            textColor: "#f5e8da",
         },
         orange: {
             logo: "images/logo-orange.png",
             backgroundColor: "#f0dfc8",
+            textColor: "#303030",
         },
     };
 
@@ -39,6 +45,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Fade out the old logo
             logo.style.opacity = 0;
+
+            // Change text color
+            textContainer.style.color = palettes[palette].textColor;
 
             // Wait for the fade out to complete
             setTimeout(() => {
